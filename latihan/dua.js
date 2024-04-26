c47e0094430557ca36c3c373352210a3e6874741
WindyAnggitaPutri <168071401+WindyAnggitaPutri@users.noreply.github.com>
2024-04-26 12:05:09 +0700

    Delete latihan directory

--- latihan/dua.js
+++
@@ -1,7 +0,0 @@
-function nama(first, last) {
-    console.log(first, last)
-}
-
-const sum = new function('x', 'y', 'console.log(x+y)')
-
-sum(5, 5)
--- latihan/tiga.js
+++
@@ -1,2 +0,0 @@
-document.getElementById('sinb').innerHTML = "sjnjsj"
-
